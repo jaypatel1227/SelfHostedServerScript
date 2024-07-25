166427050e5e69c633cf9ca0c8612e0d81936620
Jay Patel <jaypatel122700@gmail.com>
2024-07-24 22:33:58 -0500

    factored out one time commands

---
+++ fresh_setup.sh
@@ -0,0 +1,11 @@
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+cd ../
+rm -rf ../yay
+yay -Sy git rust docker docker-compose
+sudo systemctl start docker.service
+sudo systemctl enable docker.service
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+mkdir ~/Servers
--- setup.sh
+++ setup.sh
@@ -1,11 +1,3 @@
 sudo yay -Syuu
-yay -Sy git rust docker docker-compose
-sudo systemctl start docker.service
-sudo systemctl enable docker.service
-sudo groupadd docker
-sudo usermod -aG docker $USER
-newgrp docker
-
-mkdir ~/Servers
 cd ~/Servers
 git clone https://github.com/jaypatel1227/FileConversion.git

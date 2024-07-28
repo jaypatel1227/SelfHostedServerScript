37e57818e17d8aea494709f1c9f516592cfc6cec
Jay Patel <jaypatel122700@gmail.com>
2024-07-27 23:53:23 -0500

    startup script to publish docker container

--- startup.sh
+++ startup.sh
@@ -1,3 +1,9 @@
-cd ~/Servers/FileConversion/app/
-git pull
-docker compose up
+cd ~/Servers
+#cd ~/Servers/FileConversion/app/
+#git pull
+#docker compose up
+
+docker pull jaypatel1227/file-conversion
+docker run -it -p0.0.0.0:5001:5001 jaypatel1227/file-conversion
+
+docker cp ~/Servers/FileConversion/app/server_config.json docker container ls --all --filter=ancestor="jaypatel1227/file-conversion" --format "{{.ID}}":/bin/

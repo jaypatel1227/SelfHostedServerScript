d131cfd53439fb2782fae431f60b315d3ee38c30
Jay Patel <jaypatel122700@gmail.com>
2024-07-28 00:20:42 -0500

    undoing the copy since we can copy into the image directly

--- startup.sh
+++ startup.sh
@@ -5,5 +5,3 @@ cd ~/Servers
 
 docker pull jaypatel1227/file-conversion
 docker run -it -p0.0.0.0:5001:5001 jaypatel1227/file-conversion
-
-docker cp ~/Servers/FileConversion/app/server_config.json docker container ls --all --filter=ancestor="jaypatel1227/file-conversion" --format "{{.ID}}":/bin/
